bc51188de011536482b763e6f4d38e996674d1be
Leonardo Mirza <mirzaleonardo7@gmail.com>
2023-10-24 18:51:00 +0200

    a

--- app/src/main/java/com/example/empy/MainActivity.java
+++ app/src/main/java/com/example/empy/MainActivity.java
@@ -14,7 +14,6 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     WebView web;
-
     @SuppressLint({"WebViewApiAvailability", "SetJavaScriptEnabled"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
